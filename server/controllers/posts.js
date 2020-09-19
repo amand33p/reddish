37267995d8bec787761a8a010fc8ffc2ebcf0f32
Aman <amanmankoo96@gmail.com>
2020-09-19 15:04:49 +0530

    added downvote to backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -218,6 +218,50 @@ router.patch('/:id/upvote', auth, async (req, res) => {
     );
   } else {
     post.upvotedBy = post.upvotedBy.concat(user._id);
+    post.downvotedBy = post.downvotedBy.filter(
+      (d) => d.toString() !== user._id.toString()
+    );
+  }
+
+  const calculatedPoints = post.upvotedBy.length - post.downvotedBy.length;
+
+  if (calculatedPoints < 0) {
+    post.pointsCount = 0;
+  } else {
+    post.pointsCount = calculatedPoints;
+  }
+
+  await post.save();
+  res.status(202).end();
+});
+
+router.patch('/:id/downvote', auth, async (req, res) => {
+  const { id } = req.params;
+
+  const post = await Post.findById(id);
+  const user = await User.findById(req.user);
+
+  if (!post) {
+    return res.status(404).send({
+      message: `Post with ID: ${id} does not exist in database.`,
+    });
+  }
+
+  if (!user) {
+    return res
+      .status(404)
+      .send({ message: 'User does not exist in database.' });
+  }
+
+  if (post.downvotedBy.includes(user._id.toString())) {
+    post.downvotedBy = post.downvotedBy.filter(
+      (d) => d.toString() !== user._id.toString()
+    );
+  } else {
+    post.downvotedBy = post.downvotedBy.concat(user._id);
+    post.upvotedBy = post.upvotedBy.filter(
+      (u) => u.toString() !== user._id.toString()
+    );
   }
 
   const calculatedPoints = post.upvotedBy.length - post.downvotedBy.length;
